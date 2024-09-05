b3b46d554579cf1f4b05fb199d11ba16e19978cf
Korn-Sriwattana <korn_s@cmu.ac.th>
2024-09-05 16:46:56 +0700

    ข้อ1เสร็จหมดละเหลือปรับระยะ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,42 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
-
+import { 
+  Container, 
+  Space, 
+  Text, 
+  Title, 
+  Rating, 
+  Textarea,
+  Button
+} from "@mantine/core";
+import { useState } from 'react';
 export default function Home() {
+
+  const [value, setValue] = useState(0);
+
   return (
+    <>
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
+      <Space h="sm"/>
+      <Title order={4}>Your rating</Title>
+      <Rating 
+        value={value} 
+        onChange={setValue} 
+        defaultValue={0} 
+        size="28.5px"/>
+      <Space h="md"/>
+      <Textarea
+      label="Your review"
+      placeholder="Do you enjoy eating?"
+      />
+      <Button variant="filled" color="orange">
+        Submit Review
+      </Button>
 
-      <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
+      <Text ta="center" my="md">
+        Copyright © 2024 Korn Sriwattana 660610738
       </Text>
     </Container>
+    </>
   );
 }
